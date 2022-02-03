79ac0effb1eabf03e6e7fa4ba9797c3e2df068ef
Soph0298 <Soph0298@stud.kea.dk>
2022-02-03 14:37:42 +0100

    last commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#96CEB4",
+        sand: "#DED9C4",
+        darksand: "#D0CAB2",
+        button: "#89B5AF",
+        button1: "#96C7C1",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
